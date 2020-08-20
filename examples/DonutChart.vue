2cc498d4f6bb0cb08e333e41817a04fd6cca9c53
Steve McDougall <juststevemcd@gmail.com>
2020-08-20 13:07:48 +0100

    Adding some examples

---
+++ examples/DonutChart.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <vue-frappe
+      id="test"
+      title="Monthly Distribution"
+      type="donut"
+      :height="300"
+      :labels="['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']"
+      :lineOptions="{regionFill: 1}"
+      :colors="['red']"
+      :discreteDomains="false"
+      :dataSets="this.data"
+    ></vue-frappe>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      data: [
+        {
+          values: [18, 40, 30, 35, 8, 52, 17, -4],
+        },
+      ],
+    };
+  },
+};
+</script>
---
+++ examples/HeatmapChart.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+    <vue-frappe
+      id="test"
+      title="Monthly Distribution"
+      type="heatmap"
+      :colors="['#ebedf0', '#c0ddf9', '#73b3f3', '#3886e1', '#17459e']"
+      :discreteDomains="false"
+      :height="300"
+      :dataPoints="data"
+      :startDate="start"
+      :endDate="end"
+    ></vue-frappe>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  computed: {
+    start() {
+      let date = new Date();
+
+      date.setMonth(2);
+
+      return date;
+    },
+    end() {
+      let date = new Date();
+
+      date.setMonth(6);
+
+      return date;
+    },
+    data() {
+      let date1 = new Date();
+      let date2 = new Date();
+      let date3 = new Date();
+
+      date1.setMonth(2).toTimeString;
+      date2.setMonth(3).toTimeString;
+      date3.setMonth(4).toTimeString;
+
+      return {
+        date1: 30,
+        date2: 50,
+        date3: 150,
+      };
+    },
+  },
+};
+</script>
